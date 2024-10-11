9de9db91afde103685db54902290edaad8bc8016
tobias-calvo <tobias-calvo@users.noreply.github.com>
2024-10-11 00:32:40 -0300

    se agregaron imagenes, se completo la screen de MyCart y Favorites, ademas de agregar mas listas a FakeData

--- app/src/main/java/com/example/appnectar/dataClass/FakeData.kt
+++ app/src/main/java/com/example/appnectar/dataClass/FakeData.kt
@@ -3,27 +3,56 @@ package com.example.appnectar.dataClass
 import com.example.appnectar.R
 
 val ProductListItems = listOf(
-    Product(1, R.drawable.banana, "Banana", 27.5, "Test description 1", Category.FRUITS_VEGETABLES, 1, TypeSizes.kg, 10),
-    Product(2, R.drawable.pepsi, "Pepsi", 27.5, "Test description 1", Category.BEVERAGES,1, TypeSizes.L, 10),
-    Product(3, R.drawable.egg_chicken_white, "Huevos", 27.5, "Test description 1", Category.DAIRY_EGGS,1, TypeSizes.pcs, 10),
-    Product(4, R.drawable.morron, "Morron", 27.5, "Test description 1", Category.FRUITS_VEGETABLES,1, TypeSizes.kg, 10),
+    Product(1, R.drawable.banana, "Banana", 30.5, "Test description 1", Category.FRUITS_VEGETABLES, 2, TypeSizes.kg, 10),
+    Product(2, R.drawable.pepsi, "Pepsi", 15.0, "Test description 1", Category.BEVERAGES,350, TypeSizes.ml, 10),
+    Product(3, R.drawable.egg_chicken_white, "Huevos", 24.5, "Test description 1", Category.DAIRY_EGGS,6, TypeSizes.pcs, 10),
+    Product(4, R.drawable.morron, "Morron", 29.5, "Test description 1", Category.FRUITS_VEGETABLES,1, TypeSizes.kg, 10),
+    Product(5, R.drawable.sprite, "Sprite", 15.0, "Lata de Sprite", Category.BEVERAGES,350, TypeSizes.ml, 10),
+    Product(6, R.drawable.coca, "Coca-Cola", 15.0, "Lata de Coca-Cola", Category.BEVERAGES,350, TypeSizes.ml, 10),
+    Product(7, R.drawable.coca_diet, "Coca-Cola Diet", 17.5, "Lata de Coca-Cola Diet", Category.BEVERAGES,350, TypeSizes.ml, 10),
+    Product(8, R.drawable.jugo_manzana, "Jugo Manzana", 19.5, "Jugo de Manzana", Category.BEVERAGES,750, TypeSizes.ml, 10),
+    Product(9, R.drawable.mayonesa, "Mayonesa", 34.5, "Mayonesa", Category.OTHER ,1, TypeSizes.kg, 10),
+    Product(10, R.drawable.fideos, "Fideos", 32.0, "Fideos", Category.OTHER,1, TypeSizes.kg, 10),
+    Product(11, R.drawable.manzana, "Manzana", 45.0, "Manzana", Category.FRUITS_VEGETABLES,2, TypeSizes.kg, 10),
 )
 
-val FavouriteItems = listOf(
-    Product(1, R.drawable.banana, "Banana", 27.5, "Test description 1", Category.FRUITS_VEGETABLES,1, TypeSizes.kg, 10),
-    Product(2, R.drawable.pepsi, "Pepsi", 27.5, "Test description 1", Category.BEVERAGES,1, TypeSizes.L, 10),
+val FavoriteItems = listOf(
+    Product(5, R.drawable.sprite, "Sprite", 15.0, "Lata de Sprite", Category.BEVERAGES,350, TypeSizes.ml, 10),
+    Product(7, R.drawable.coca_diet, "Coca-Cola Diet", 17.5, "Lata de Coca-Cola Diet", Category.BEVERAGES,350, TypeSizes.ml, 10),
+    Product(8, R.drawable.jugo_manzana, "Jugo Manzana", 19.5, "Jugo de Manzana", Category.BEVERAGES,750, TypeSizes.ml, 10),
+    Product(6, R.drawable.coca, "Coca-Cola", 15.0, "Lata de Coca-Cola", Category.BEVERAGES,350, TypeSizes.ml, 10),
+    Product(2, R.drawable.pepsi, "Pepsi", 15.0, "Test description 1", Category.BEVERAGES,350, TypeSizes.ml, 10),
+    Product(3, R.drawable.egg_chicken_white, "Huevos", 24.5, "Test description 1", Category.DAIRY_EGGS,6, TypeSizes.pcs, 10),
+    Product(9, R.drawable.mayonesa, "Mayonesa", 34.5, "Mayonesa", Category.OTHER ,1, TypeSizes.kg, 10),
+    Product(10, R.drawable.fideos, "Fideos", 32.0, "Fideos", Category.OTHER,1, TypeSizes.kg, 10),
+    Product(11, R.drawable.manzana, "Manzana", 45.0, "Manzana", Category.FRUITS_VEGETABLES,2, TypeSizes.kg, 10),
 )
 
 val ExclusiveOffer = listOf(
-    Product(1, R.drawable.banana, "Banana", 27.5, "Test description 1", Category.FRUITS_VEGETABLES, 1, TypeSizes.kg, 10),
-    Product(2, R.drawable.pepsi, "Pepsi", 27.5, "Test description 1", Category.BEVERAGES,1, TypeSizes.L, 10),
-    Product(3, R.drawable.egg_chicken_white, "Huevos", 27.5, "Test description 1", Category.DAIRY_EGGS,1, TypeSizes.pcs, 10),
-    Product(4, R.drawable.morron, "Morron", 27.5, "Test description 1", Category.FRUITS_VEGETABLES,1, TypeSizes.kg, 10),
+    Product(1, R.drawable.banana, "Banana", 27.5, "Test description 1", Category.FRUITS_VEGETABLES, 2, TypeSizes.kg, 10),
+    Product(10, R.drawable.manzana, "Manzana", 45.0, "Manzana", Category.FRUITS_VEGETABLES,2, TypeSizes.kg, 10),
+    Product(2, R.drawable.pepsi, "Pepsi", 15.0, "Test description 1", Category.BEVERAGES,350, TypeSizes.ml, 10),
+    Product(4, R.drawable.morron, "Morron", 29.5, "Test description 1", Category.FRUITS_VEGETABLES,1, TypeSizes.kg, 10),
+    Product(5, R.drawable.sprite, "Sprite", 15.0, "Lata de Sprite", Category.BEVERAGES,350, TypeSizes.ml, 10),
+    Product(9, R.drawable.mayonesa, "Mayonesa", 34.5, "Mayonesa", Category.OTHER ,1, TypeSizes.kg, 10),
+    Product(10, R.drawable.fideos, "Fideos", 32.0, "Fideos", Category.OTHER,1, TypeSizes.kg, 10),
 )
 
 val BestSelling = listOf(
-    Product(1, R.drawable.banana, "Banana", 27.5, "Test description 1", Category.FRUITS_VEGETABLES, 1, TypeSizes.kg, 10),
-    Product(2, R.drawable.pepsi, "Pepsi", 27.5, "Test description 1", Category.BEVERAGES,1, TypeSizes.L, 10),
-    Product(3, R.drawable.egg_chicken_white, "Huevos", 27.5, "Test description 1", Category.DAIRY_EGGS,1, TypeSizes.pcs, 10),
-    Product(4, R.drawable.morron, "Morron", 27.5, "Test description 1", Category.FRUITS_VEGETABLES,1, TypeSizes.kg, 10),
+    Product(4, R.drawable.morron, "Morron", 29.5, "Test description 1", Category.FRUITS_VEGETABLES,1, TypeSizes.kg, 10),
+    Product(7, R.drawable.coca_diet, "Coca-Cola Diet", 17.5, "Lata de Coca-Cola Diet", Category.BEVERAGES,350, TypeSizes.ml, 10),
+    Product(8, R.drawable.jugo_manzana, "Jugo Manzana", 19.5, "Jugo de Manzana", Category.BEVERAGES,750, TypeSizes.ml, 10),
+    Product(9, R.drawable.mayonesa, "Mayonesa", 34.5, "Mayonesa", Category.OTHER ,1, TypeSizes.kg, 10),
+    Product(10, R.drawable.fideos, "Fideos", 32.0, "Fideos", Category.OTHER,1, TypeSizes.kg, 10),
+)
+
+val MyCarts = listOf(
+    Product(4, R.drawable.morron, "Morron", 29.5, "Test description 1", Category.FRUITS_VEGETABLES,1, TypeSizes.kg, 10),
+    Product(3, R.drawable.egg_chicken_white, "Huevos", 24.5, "Test description 1", Category.DAIRY_EGGS,6, TypeSizes.pcs, 10),
+    Product(1, R.drawable.banana, "Banana", 27.5, "Test description 1", Category.FRUITS_VEGETABLES, 2, TypeSizes.kg, 10),
+    Product(2, R.drawable.pepsi, "Pepsi", 15.0, "Test description 1", Category.BEVERAGES,350, TypeSizes.ml, 10),
+    Product(5, R.drawable.sprite, "Sprite", 15.0, "Lata de Sprite", Category.BEVERAGES,350, TypeSizes.ml, 10),
+    Product(7, R.drawable.coca_diet, "Coca-Cola Diet", 17.5, "Lata de Coca-Cola Diet", Category.BEVERAGES,350, TypeSizes.ml, 10),
+    Product(9, R.drawable.mayonesa, "Mayonesa", 34.5, "Mayonesa", Category.OTHER ,1, TypeSizes.kg, 10),
+    Product(10, R.drawable.fideos, "Fideos", 32.0, "Fideos", Category.OTHER,1, TypeSizes.kg, 10),
 )
